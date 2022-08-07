b1c9d41e44e0ceed7d674c1f37f0851cf930cbd3
KamleshSirvi <110775078+KamleshSirvi@users.noreply.github.com>
2022-08-07 20:04:54 +0530

    Delete DeleteNode.java

--- DeleteNode.java
+++
@@ -1,9 +0,0 @@
-public void deleteNode(ListNode node) {
-        
-        node.val = node.next.val;
-        if(node.next.next != null){
-            node.next = node.next.next;
-        }else{
-            node.next = null;
-        }
-    }
