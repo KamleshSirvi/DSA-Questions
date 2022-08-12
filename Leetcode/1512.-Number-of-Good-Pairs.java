6156273bb82e0d4176bcdff9d5707451feff889c
KamleshSirvi <110775078+KamleshSirvi@users.noreply.github.com>
2022-08-12 09:52:39 +0530

    Update 1512.-Number-of-Good-Pairs.java

--- Leetcode/1512.-Number-of-Good-Pairs.java
+++ Leetcode/1512.-Number-of-Good-Pairs.java
@@ -1,3 +1,16 @@
+// using Hash Map
+public int numIdenticalPairs(int[] nums) {
+        // using hashTable
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                count = count + map.get(nums[i]);
+            }
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        return count;
+}
 
 // naive solution
 public int numIdenticalPairs(int[] nums) {
