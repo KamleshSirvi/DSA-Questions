9aec082a6237aa0d83917ac33433508a2ba2e74a
KamleshSirvi <110775078+KamleshSirvi@users.noreply.github.com>
2022-08-12 13:54:54 +0530

    Update 141.-Linked-List-Cycle.java

--- Leetcode/141.-Linked-List-Cycle.java
+++ Leetcode/141.-Linked-List-Cycle.java
@@ -1,3 +1,24 @@
+// using two pointer approach
+public boolean hasCycle(ListNode head) {
+        // Two pointer approach
+        ListNode slow = head;
+        ListNode fast = head;
+        int flag = 0;
+        while(slow != null && fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 1){
+            return true;
+        }else{
+            return false;
+        }
+}
+
 // using HashMap
 public boolean hasCycle(ListNode head) {
         HashSet<ListNode> set = new HashSet<>();
