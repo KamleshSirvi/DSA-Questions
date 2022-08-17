d9770c54663fc4095036477c25f07141d36138b3
KamleshSirvi <110775078+KamleshSirvi@users.noreply.github.com>
2022-08-17 09:59:40 +0530

    Update 1.-Two-Sum.java

--- Leetcode/1.-Two-Sum.java
+++ Leetcode/1.-Two-Sum.java
@@ -1,24 +1,30 @@
-// using hashmap 
+// Efficient Approach using hashMap
+
 public int[] twoSum(int[] nums, int target) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int x : nums){
-            set.add(x);
-            if(set.contains(target - x)){
-                return true;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // create a array for store i and j
+        int[] res = new int[2];
+        
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+                return res;
             }
+            map.put(nums[i], i);
         }
-        return false;
-}
+        return null;
+    }
 
 // using naive solution
 public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] == target - nums[i]) {
-                    return new int[] { i, j };
-                }
-            }
-        }
-        // In case there is no solution, we'll just return null
-        return null;
+         for (int i = 0; i < nums.length; i++) {
+             for (int j = i + 1; j < nums.length; j++) {
+                 if (nums[j] == target - nums[i]) {
+                     return new int[] { i, j };
+                 }
+             }
+         }
+//          In case there is no solution, we'll just return null
+         return null;
     }
